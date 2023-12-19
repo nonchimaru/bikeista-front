a21a6c21c3e232daaff2490d518b9ff61caa76d6
nonchimaru <non.ari0523@gmail.com>
2023-12-19 16:56:30 +0900

    ロゴとアイテムリストのコンポーネントファイルの作成

---
+++ src/components/features/home_b/components/ItemList.tsx
@@ -0,0 +1,60 @@
+import Link from "next/link";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
+
+/**
+ * ロゴを表示させるコンポーネント
+ */
+export const ItemList: React.FC = ({}) => {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        bgcolor: "background.paper",
+      }}
+    >
+      <List>
+        <ListItem disablePadding>
+          <Link href="/" style={{ color: "black", textDecoration: "none" }}>
+            <ListItemButton>
+              <ListItemText primary="愛車同盟" />
+            </ListItemButton>
+          </Link>
+        </ListItem>
+        <ListItem disablePadding>
+          <Link href="/" style={{ color: "black", textDecoration: "none" }}>
+            <ListItemButton>
+              <ListItemText primary="ピク投稿" />
+            </ListItemButton>
+          </Link>
+        </ListItem>
+        <ListItem disablePadding>
+          <Link href="/" style={{ color: "black", textDecoration: "none" }}>
+            <ListItemButton>
+              <ListItemText primary="レタ投稿" />
+            </ListItemButton>
+          </Link>
+        </ListItem>
+        <ListItem disablePadding>
+          <Link href="/" style={{ color: "black", textDecoration: "none" }}>
+            <ListItemButton>
+              <ListItemText primary="メッセージ" />
+            </ListItemButton>
+          </Link>
+        </ListItem>
+        <ListItem disablePadding>
+          <Link href="/" style={{ color: "black", textDecoration: "none" }}>
+            <ListItemButton>
+              <ListItemText primary="マイガレージ" />
+            </ListItemButton>
+          </Link>
+        </ListItem>
+      </List>
+    </Box>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,11 @@
+import { NextPage } from "next";
 import Head from "next/head";
 import * as React from "react";
-import Avatar from "@mui/material/Avatar";
-import {
-  Box,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemText,
-  Typography,
-} from "@mui/material";
-import Link from "next/link";
+import { Box, Typography } from "@mui/material";
+import { LogoBikeista } from "@/components/features/home_b/components/LogoBikeista";
+import { ItemList } from "@/components/features/home_b/components/itemlist";
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
     <>
       <Head>
@@ -19,81 +13,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Avatar
-          alt="Logo"
-          src="/logo_2.jpg"
-          variant="square"
-          sx={{ width: "10%", height: "10%" }}
-        />
+        <LogoBikeista />
         <Box>
           <Typography variant="h4">
             Welcome to <br />
           </Typography>
           <Typography variant="h2">BIKEISTA</Typography>
         </Box>
-        <Box
-          sx={{
-            width: "100%",
-            bgcolor: "background.paper",
-            paddingLeft: "5%",
-          }}
-        >
-          <nav aria-label="main mailbox folders">
-            <List>
-              <ListItem disablePadding>
-                <Link
-                  href="/"
-                  style={{ color: "black", textDecoration: "none" }}
-                >
-                  <ListItemButton>
-                    <ListItemText primary="愛車同盟" />
-                  </ListItemButton>
-                </Link>
-              </ListItem>
-              <ListItem disablePadding>
-                <Link
-                  href="/"
-                  style={{ color: "black", textDecoration: "none" }}
-                >
-                  <ListItemButton>
-                    <ListItemText primary="ピク投稿" />
-                  </ListItemButton>
-                </Link>
-              </ListItem>
-              <ListItem disablePadding>
-                <Link
-                  href="/"
-                  style={{ color: "black", textDecoration: "none" }}
-                >
-                  <ListItemButton>
-                    <ListItemText primary="レタ投稿" />
-                  </ListItemButton>
-                </Link>
-              </ListItem>
-              <ListItem disablePadding>
-                <Link
-                  href="/"
-                  style={{ color: "black", textDecoration: "none" }}
-                >
-                  <ListItemButton>
-                    <ListItemText primary="メッセージ" />
-                  </ListItemButton>
-                </Link>
-              </ListItem>
-              <ListItem disablePadding>
-                <Link
-                  href="/"
-                  style={{ color: "black", textDecoration: "none" }}
-                >
-                  <ListItemButton>
-                    <ListItemText primary="マイガレージ" />
-                  </ListItemButton>
-                </Link>
-              </ListItem>
-            </List>
-          </nav>
-        </Box>
+        <ItemList />
       </main>
     </>
   );
-}
+};
+
+export default Home;
