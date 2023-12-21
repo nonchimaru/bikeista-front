603c5b9bd20892ff7e4448a974b0b9e5b8b3d8e6
nonchimaru <non.ari0523@gmail.com>
2023-12-21 17:23:39 +0900

    フォントの変更と配置調整

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,23 @@ import Head from "next/head";
 import * as React from "react";
 import { Box, Typography } from "@mui/material";
 import { LogoBikeista } from "@/components/features/home_b/components/LogoBikeista";
-import { ItemList } from "@/components/features/home_b/components/itemlist";
+import { Zen_Antique, Kaisei_Decol, M_PLUS_Rounded_1c } from "next/font/google";
+import { ItemList } from "@/components/features/home_b/components/ItemList";
+
+const Zen_Antique400 = Zen_Antique({
+  weight: "400",
+  preload: false,
+});
+
+const Kaisei_Decol700 = Kaisei_Decol({
+  weight: "700",
+  preload: false,
+});
+
+const M_PLUS_Rounded_1c500 = M_PLUS_Rounded_1c({
+  weight: "500",
+  preload: false,
+});
 
 const Home: NextPage = () => {
   return (
@@ -14,13 +30,18 @@ const Home: NextPage = () => {
       </Head>
       <main>
         <LogoBikeista />
-        <Box>
-          <Typography variant="h4">
+
+        <Box style={{ marginLeft: "5%" }}>
+          <Typography variant="h3" className={M_PLUS_Rounded_1c500.className}>
             Welcome to <br />
           </Typography>
-          <Typography variant="h2">BIKEISTA</Typography>
+          <Typography variant="h1" className={Kaisei_Decol700.className}>
+            BIKEISTA
+          </Typography>
+          <Box style={{ marginTop: "2%" }}>
+            <ItemList />
+          </Box>
         </Box>
-        <ItemList />
       </main>
     </>
   );
