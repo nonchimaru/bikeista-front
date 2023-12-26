1e39b903cf696b9481a57e9cab77624ec1525acc
nonchimaru <non.ari0523@gmail.com>
2023-12-26 16:37:00 +0900

    画像のスライドショーの実装

---
+++ src/components/features/home_b/components/PhotoSlide.tsx
@@ -0,0 +1,47 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+
+const sliderSettings = {
+  autoplay: true, // スライドを自動再生
+  autoplaySpeed: 3000,
+  infinite: true, // コンテンツループ
+  arrows: false, // 「前」「次」のスライドを操作する矢印を非表示
+  speed: 500, // スライドアニメーションのスピード
+  variableWidth: false,
+  draggable: false,
+  cssEase: "linear",
+  fade: true,
+  slidesToShow: 3,
+};
+
+const carouselItems = [
+  { image: "https://picsum.photos/1200?random=1", alt: "" },
+  { image: "https://picsum.photos/1200?random=2", alt: "" },
+  { image: "https://picsum.photos/1200?random=3", alt: "" },
+];
+
+const sliderContainerStyle = {
+  width: "300px", // 任意の幅に調整
+  margin: "0 auto", // 中央寄せにする場合
+};
+
+/**
+ * ロゴを表示させるコンポーネント
+ */
+export const PhotoSlide: React.FC = ({}) => {
+  return (
+    <div style={sliderContainerStyle}>
+      <Slider {...sliderSettings}>
+        {carouselItems.map((carouselItem, index) => (
+          <div key={index}>
+            <img
+              src={carouselItem.image}
+              alt={carouselItem.alt}
+              width="100%" // 画像の幅を100%にすることでスライダーに合わせる
+            />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,9 @@ import Head from "next/head";
 import * as React from "react";
 import { Box, Typography } from "@mui/material";
 import { LogoBikeista } from "@/components/features/home_b/components/LogoBikeista";
-import { Zen_Antique, Kaisei_Decol, M_PLUS_Rounded_1c } from "next/font/google";
+import { Kaisei_Decol, M_PLUS_Rounded_1c } from "next/font/google";
 import { ItemList } from "@/components/features/home_b/components/ItemList";
-
-const Zen_Antique400 = Zen_Antique({
-  weight: "400",
-  preload: false,
-});
+import { PhotoSlide } from "@/components/features/home_b/components/PhotoSlide";
 
 const Kaisei_Decol700 = Kaisei_Decol({
   weight: "700",
@@ -27,20 +23,23 @@ const Home: NextPage = () => {
       <Head>
         <title>BIKEISTA</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="./style.css" />
       </Head>
       <main>
         <LogoBikeista />
-
-        <Box style={{ marginLeft: "5%" }}>
-          <Typography variant="h3" className={M_PLUS_Rounded_1c500.className}>
-            Welcome to <br />
-          </Typography>
-          <Typography variant="h1" className={Kaisei_Decol700.className}>
-            BIKEISTA
-          </Typography>
-          <Box style={{ marginTop: "2%" }}>
-            <ItemList />
+        <Box style={{ display: "flex" }}>
+          <Box style={{ marginLeft: "5%" }}>
+            <Typography variant="h3" className={M_PLUS_Rounded_1c500.className}>
+              Welcome to <br />
+            </Typography>
+            <Typography variant="h1" className={Kaisei_Decol700.className}>
+              BIKEISTA
+            </Typography>
+            <Box style={{ marginTop: "2%" }}>
+              <ItemList />
+            </Box>
           </Box>
+          <PhotoSlide />
         </Box>
       </main>
     </>
