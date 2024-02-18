652190d2fe383b4954e0bc38a635c86f37268474
nonchimaru <non.ari0523@gmail.com>
2024-02-18 23:00:35 +0900

    名前データの取得

--- src/components/features/My_Garage/components/Name.tsx
+++ src/components/features/My_Garage/components/Name.tsx
@@ -2,7 +2,7 @@ import { Box, Typography } from "@mui/material";
 
 type NameProps = {
   userName: string;
-  userID: string;
+  userId: string;
 };
 
 /**
@@ -10,8 +10,8 @@ type NameProps = {
  */
 export const Name: React.FC<NameProps> = (props) => {
   return (
-    <Box sx={{ marginLeft: 5, marginTop: "3%" }}>
-      <Typography fontSize="h6.fontSize">{props.userID}</Typography>
+    <Box sx={{ marginLeft: 5, marginTop: "9%" }}>
+      <Typography fontSize="h6.fontSize">{props.userId}</Typography>
       <Typography fontSize="h4.fontSize" sx={{ lineHeight: 1.5 }}>
         {props.userName}
       </Typography>
