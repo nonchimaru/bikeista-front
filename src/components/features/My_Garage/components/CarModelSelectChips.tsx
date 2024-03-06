ee241c0b7bdc66a80dd67b6e32e9bce64f2a9e4c
nonchimaru <non.ari0523@gmail.com>
2024-03-06 16:49:38 +0900

    微調整をする

--- src/components/features/My_Garage/components/CarModelSelectChips.tsx
+++ src/components/features/My_Garage/components/CarModelSelectChips.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { CarModel } from "./ProfileModal";
 
 const columns: GridColDef[] = [
@@ -18,18 +18,6 @@ const columns: GridColDef[] = [
   },
 ];
 
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 14 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 31 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 31 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 11 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-];
-
 interface CarModelSelectChipsProps {
   carModelSelectChip: CarModel[];
 }
@@ -37,10 +25,11 @@ interface CarModelSelectChipsProps {
 export const CarModelSelectChips: React.FC<CarModelSelectChipsProps> = (
   props
 ) => {
+  const { carModelSelectChip } = props;
   return (
     <Box sx={{ height: 400, width: "100%" }}>
       <DataGrid
-        rows={props.carModelSelectChip}
+        rows={carModelSelectChip}
         columns={columns}
         initialState={{
           pagination: {
