b467114dceeb91386a90c4fae347e945235685cd
nonchimaru <non.ari0523@gmail.com>
2024-03-13 00:11:37 +0900

    全然反映されないぜ

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+// import "./globals.css";
+// import { M_PLUS_Rounded_1c } from "next/font/google";
+
+// const M_PLUS_Rounded_1c500 = M_PLUS_Rounded_1c({
+//   weight: "500",
+//   preload: false,
+// });
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en">
+//       <body className={M_PLUS_Rounded_1c500.className}>{children}</body>
+//     </html>
+//   );
+// }
