b04e2c384085b8edf3ac9c18eb992971036c9a69
Jumpei-Arigaya <junpeeei0407@gmail.com>
2024-03-13 17:07:25 +0900

    MUIの最新バージョンに対応

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,6 @@
-import { createMuiTheme } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 
-const theme = createMuiTheme({
+const theme = createTheme({
   typography: {
     fontFamily: ["M PLUS Rounded 1c", "sans-serif"].join(","),
     fontWeightRegular: 500,
